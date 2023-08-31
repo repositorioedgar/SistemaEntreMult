4ab54b9ce17a5557ee7f45e49870dfafc8b99264
repositorioedgar <142647158+repositorioedgar@users.noreply.github.com>
2023-08-31 12:23:41 -0400

    Add files via upload

---
+++ SistemaEntreMult/DVD.java
@@ -0,0 +1,23 @@
+
+/**
+ * Univ. Edgar Condori Polloquiri
+ */
+public class DVD extends Elemento
+{
+    private String director;
+    public DVD(String elTitulo, String elDirector, int tiempo)
+    {
+        super(elTitulo, tiempo);
+        director = elDirector;
+    }
+    public String getDirector()
+    {
+        return director;
+    }
+    public void print()
+    {
+        System.out.println("DVD");
+        System.out.println("Titulo"+ getTitulo());
+        System.out.println("Director"+ director);
+    }
+}
